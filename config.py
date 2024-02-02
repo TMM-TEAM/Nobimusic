717ab504b530f2f5e8bd22f3b0f37fcd238dab45
TMM-TEAM <150228624+TMM-TEAM@users.noreply.github.com>
2024-02-02 12:52:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/DAXXMUSIC",
+    "https://github.com/TMM-TEAM/Nobimusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
